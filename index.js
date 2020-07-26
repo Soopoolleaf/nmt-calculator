eeecba6ec9b1bc59e08dc6c4745188cbacf0d6bd
Soopoolleaf <59555129+Soopoolleaf@users.noreply.github.com>
2020-07-26 22:43:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 "use strict";
 
 //List of species and their number of villagers.
-var species = { "Alligator": 7, "Anteater": 7, "Bear": 15, "Bird": 13, "Bull": 6, "Cat": 23, "Chicken": 9, "Cow": 4, "Cub": 16, "Deer": 10, "Dog": 16, "Duck": 17, "Eagle": 9, "Elephant": 11, "Frog": 18, "Goat": 8, "Gorilla": 9, "Hamster": 8, "Hippo": 7, "Horse": 15, "Kangaroo": 8, "Koala": 9, "Lion": 7, "Monkey": 8, "Mouse": 15, "Octopus": 3, "Ostrich": 10, "Penguin": 13, "Pig": 15, "Rabbit": 20, "Rhino": 6, "Sheep": 13, "Squirrel": 18, "Tiger": 7, "Wolf": 11 };
+var species = { "강아지": 16, 
+"개구리": 18, 
+"개미핥기": 7, 
+"고릴라": 9, 
+"고양이": 23, 
+"곰": 15, 
+"늑대": 11, 
+"다람쥐": 18, 
+"닭": 9, 
+"독수리": 9, 
+"돼지": 15, 
+"말": 15, 
+"문어": 3, 
+"사슴": 10, 
+"사자": 7, 
+"새": 13, 
+"수소": 6, 
+"아기곰": 16, 
+"악어": 7, 
+"암소": 4, 
+"양": 13, 
+"염소": 8, 
+"오리": 17, 
+"원숭이": 8, 
+"쥐": 15, 
+"캥거루": 8, 
+"코끼리": 11, 
+"코뿔소": 6, 
+"코알라": 9, 
+"타조": 10, 
+"토끼": 20, 
+"펭귄": 13, 
+"하마": 7, 
+"햄스터": 8, 
+"호랑이": 7, 
+};
 
 var speciesSelector = document.getElementById('species-select');
 var result = document.getElementById('result');
@@ -13,7 +48,7 @@ var myChart = new Chart(ctx, {
     type: 'line',
     data: {
         datasets: [{
-            label: 'Probabilities',
+            label: '확률',
             data: [],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
@@ -28,7 +63,7 @@ var myChart = new Chart(ctx, {
     options: {
         title: {
             display: true,
-            text: "Probabilities/number of tickets"
+            text: "확률/티켓 수"
         } 
     }
 });
@@ -77,15 +112,15 @@ function calculateVillagersOdds() {
 
     //Check if the percent is positive and not equal to 0. Which would mean that the number of villager entered is greater than the number within the species.
     if (Math.sign(percent) !== -1 && Math.sign(percent) !== 0 && isFinite(percent)) {
-        result.innerHTML = "You have a <b>" + percent + "%</b> chance (<b>1:" + Math.round(fractionalOdd) + "</b>) of finding a specific <b>" + selectedSpecies + "</b> per Nook Miles Ticket and a <b>" + (1/numberOfSpecies * 100).toFixed(decimalPlaces) + "%</b> chance (<b>1:" + numberOfSpecies + "</b>) of finding any <b>" + selectedSpecies + "</b>.";
-        currentFormula.innerHTML = "<br><br>Current formula: 1/" + numberOfSpecies + " * 1/(" + numberOfVillager + "-" + numberOwned + ")"; 
+        result.innerHTML = "당신이 게임 플레이를 하며 <b> "+selectedSpecies +"</b>" + " 종 한 마리를 만날 확률은 <b>" + percent + "%</b> (<b>1:" + Math.round(fractionalOdd) + "</b>) 입니다. <br><br>마일 티켓 하나 당 <b>" + (1/numberOfSpecies * 100).toFixed(decimalPlaces) + "%</b>  (<b>1:" + numberOfSpecies + "</b>) 확률을 가지고 있습니다.  <b></b>";
+        currentFormula.innerHTML = "<br><br>현재 공식: 1/" + numberOfSpecies + " * 1/(" + numberOfVillager + "-" + numberOwned + ")"; 
 
         //Check if the entered number of NMT is greater than 1. Otherwise the result would be the same as the line above.
         if (Math.sign(nmtNumber) !== -1 && Math.sign(nmtNumber) !== 0 && nmtNumber !== 1 && !isNaN(nmtNumber)) {
-            result.innerHTML += "<br><br>If you use <b>" + nmtNumber + "</b> Nook Miles Tickets, you have a <b>" + bernoulli + "%</b> chance of encountering said villager.";
+            result.innerHTML += "<br><br>마일 티켓 <b>"+nmtNumber+"</b>개를 쓸 것이라면, 해당 주민을 만날 확률은<b>" + bernoulli + "%</b> 입니다. ";
         }
     } else {
-        result.innerHTML = "The number you entered is greater than the number of villagers within that species.";
+        result.innerHTML = "입력한 수가 해당 종 수보다 많습니다. ";
         currentFormula.innerHTML = "";
     }
 
